a9d2de35bfcd2adeb1de9ad60bf7a6e3b5afc70c
Martinrc93 <martin12081993@gmail.com>
2025-09-10 17:19:07 -0300

    update 10/09

--- js/main.js
+++ js/main.js
@@ -6,4 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.classList.toggle('nav-active');
         });
     }
+
+    const dropdowns = document.querySelectorAll('.dropdown > a');
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('click', (e) => {
+            if (document.body.classList.contains('nav-active')) {
+                e.preventDefault();
+                const parent = dropdown.parentElement;
+                parent.classList.toggle('submenu-active');
+            }
+        });
+    });
 });
